b591d406634ed1475168254f51cd30fa2cf3dfee
djacquel <github@pygmeeweb.com>
2018-05-25 09:47:44 +0200

    restricted automatic category setting to KB articles only

--- _plugins/hook-collection-category-from-path.rb
+++ _plugins/hook-collection-category-from-path.rb
@@ -8,12 +8,19 @@
 # we get category => path and this category is automatically merged to categories array
 # this avoid messing with large hierarchy
 # and having to set default configuration
+# This is only done in collections where isKB == true
 # Limitation is that it only takes one level into account
 # we cannot do _mycollection/my/path to get categories [my, path]
 
 Jekyll::Hooks.register :documents, :post_init do |document, payload|
-  folder = document.relative_path
-  cat = folder.split("/")
-  document.data['category'] = cat[1]
-  puts("assigned category #{cat[1]} to #{folder}")
+  isArticle = document.collection.metadata['isKB']
+  path = document.relative_path
+  if isArticle
+    cat = path.split("/")
+    document.data['category'] = cat[1]
+    puts("assigned category #{cat[1]} to #{path}")
+  else
+    puts("no category from path assigned to #{path}")
+  end
+
 end
