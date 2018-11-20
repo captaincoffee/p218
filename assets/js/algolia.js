1aa7aefc3a48f927da9a48f52742dfea093f59a9
djacquel <github@pygmeeweb.com>
2018-11-20 13:54:33 +0100

    Trying to improve search experience. Still a long way to go ...

--- assets/js/algolia.js
+++ assets/js/algolia.js
@@ -10,6 +10,7 @@ $('#search-results').on('shown.bs.modal', function () {
   $('#search-box .ais-search-box--input').trigger('focus')
 })
 
+
 // and we need to erase search box when modal closes
 // $('#search-results').on('hide.bs.modal  ', function () {
 //   console.log("hide.bs.modal on '#search-results' : clearing search box")
@@ -19,42 +20,54 @@ $('#search-results').on('shown.bs.modal', function () {
 //   // console.log(search.helper.setQuery(""))
 // })
 
-// algolia search
-// instantiate the v3 way
-// https://community.algolia.com/instantsearch.js/v2/guides/prepare-for-v3.html#new-usage
-
-
-
 const search = instantsearch({
   indexName: '{{ site.algolia.index_name }}',
   searchClient: algoliasearch('{{ site.algolia.application_id }}', '{{ site.algolia.search_only_api_key }}'),
-  routing: true,
+  routing: false,
   searchParameters: {
-    typoTolerance: false
+    typoTolerance: false,
+    hitsPerPage: 10,
   },
   searchFunction: function(helper){
-    let q = helper.state.query;
-    let minSearchLength = {{ site.customAlgolia.minSearchLength }};
-    $('#search-query').html(q);
     console.log("++++++++++++++++++ searchFunction")
-    console.log("Current query : " + q);
-    if (q.length >= minSearchLength) {
-      console.log("Querying server for : " + q);
-      helper.setQuery(q)
-          .setPage(0)
-          .search();
-    } else {
-      console.log('Minimal Query length to search = ' + minSearchLength)
-      // In case we are in a clear all process (see instantsearch.widgets.clearAll bellow)
-      // search box is not cleared
-      // hits are not cleared
-      helper.setQuery("").setPage(0);
-    }
+    // error: triggered when Algolia sends back an error
+    helper.on('change', function(content) {
+      console.log('helper on change')
+    });
+    // search: triggered when the search is sent to Algolia
+    helper.on('search', function(content) {
+      console.log('helper on search')
+    });
+    // result: triggered when the results are retrieved Algolia
+    helper.on('result', function(content) {
+      console.log('helper on result')
+    });
+    // change: triggered when a parameter is set or updated
+    helper.on('error', function(content) {
+      console.log('helper on error')
+    });
 
-
+    let q = helper.state.query
+    let minSearchLength = {{ site.customAlgolia.minSearchLength }}
+    $('#search-query').html(q)
+    console.log("Current query : " + q)
+    helper.search()
     console.log("-------------------- END searchFunction")
   }
 });
+
+const searchBox = instantsearch.widgets.searchBox({
+  container: '#search-box',
+  autofocus: true,
+  placeholder: 'Search P2 ...',
+  reset: true,
+  poweredBy: true,
+  magnifier: true,
+  loadingIndicator: true,
+  searchOnEnterKeyPressOnly: false
+})
+
+search.addWidget( searchBox )
 
 // hits
 search.addWidget(
@@ -63,11 +76,13 @@ search.addWidget(
     templates: {
       empty: 'No results',
       item: function(hit) {
-        //console.log(hit);
+        // console.log(hit)
+        const date = new Date(hit.date * 1000);
+        const dateFormatted = date.toISOString().slice(0,10);
         return `
           <div class="post-item">
-            <span class="post-meta">${hit.date}</span>
-            <h2><a class="post-link" href="{{ site.baseurl }}${hit.url}">${hit._highlightResult.title.value}</a></h2>
+            <span class="post-meta">${dateFormatted}</span>
+            <h4><a class="post-link" href="{{ site.baseurl }}${hit.url}">${hit._highlightResult.title.value}</a></h4>
             <div class="post-snippet">${hit._highlightResult.html.value}</div>
           </div>
         `;
@@ -75,31 +90,21 @@ search.addWidget(
     }
   })
 );
-// searchBox
+
+//refinementList
 search.addWidget(
-  instantsearch.widgets.searchBox({
-    container: '#search-box',
-    autofocus: true,
-    placeholder: 'Search P2 ...',
-    reset: true,
-    poweredBy: true,
-    magnifier: true,
-    loadingIndicator: true,
-    searchOnEnterKeyPressOnly: false
+  instantsearch.widgets.refinementList({
+    container: '#refinement-list2',
+    attributeName: 'categories',
+    sortBy: [ "isRefined", "name:asc" ],
+    collapsible: true,
+    autoHideContainer: true,
+    operator: 'or',
+    templates: {
+      header: 'Category (click to hide)'
+    }
   })
 );
-//refinementList
-// search.addWidget(
-//   instantsearch.widgets.refinementList({
-//     container: '#refinement-list2',
-//     attributeName: 'categories',
-//     collapsible: true,
-//     operator: 'or',
-//     templates: {
-//       header: 'Category'
-//     }
-//   })
-// );
 //currentRefinedValues
 // search.addWidget(
 //   instantsearch.widgets.currentRefinedValues({
@@ -117,37 +122,37 @@ search.addWidget(
 //   })
 // );
 // initialize pagination
-// search.addWidget(
-//   instantsearch.widgets.pagination({
-//     container: '#pagination',
-//     maxPages: 20,
-//     // default is to scroll to 'body', here we disable this behavior
-//     scrollTo: false
-//   })
-// );
+search.addWidget(
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+    maxPages: 10,
+    // default is to scroll to 'body', here we disable this behavior
+    scrollTo: false
+  })
+);
 //hitsPerPageSelector
 // search.addWidget(
 //   instantsearch.widgets.hitsPerPageSelector({
 //     container: '#hits-per-page-selector',
 //     items: [
-//       {value: 3, label: '3 per page', default: true},
-//       {value: 6, label: '6 per page'},
-//       {value: 12, label: '12 per page'},
+//       {value: 2, label: '2 per page', default: true},
+//       {value: 20, label: '20 per page'},
+//       {value: 50, label: '50 per page'},
 //     ]
 //   })
 // );
 
 // initialize clearAll
-search.addWidget(
-  instantsearch.widgets.clearAll({
-    container: '#clear-all',
-    templates: {
-      link: 'Reset everything'
-    },
-    autoHideContainer: false,
-    clearsQuery: true
-  })
-);
+// search.addWidget(
+//   instantsearch.widgets.clearAll({
+//     container: '#clear-all',
+//     templates: {
+//       link: 'Reset everything'
+//     },
+//     autoHideContainer: true,
+//     clearsQuery: true
+//   })
+// );
 
 search.start();
 
