ad183754789e9e0c9e025c7bb8e7f3a0512a7720
djacquel <github@pygmeeweb.com>
2018-11-22 08:52:30 +0100

    Working with date_added_kb + date to time plugin edit

--- _plugins/hook-kb-add-date-kb-added.rb
+++ _plugins/hook-kb-add-date-kb-added.rb
@@ -1,6 +1,6 @@
 Jekyll::Hooks.register :documents, :pre_render do |document, payload|
 
-=begin
+=begin ################################################
   Fill date_added_kb front matter field if necessary
   AND
   Ensure that all date_added_kb are Time objects
@@ -10,15 +10,11 @@ Jekyll::Hooks.register :documents, :pre_render do |document, payload|
 
   # only for knowledge base documents
   if document.collection.metadata['isKB']
-
     date       = document.data['date']
     date_added = document.data['date_added_kb']
-
     if date_added.nil? || date_added.kind_of?(Date)
       date_added = date.to_time
       document.data['date_added_kb'] = date_added
     end
-
-    Jekyll.logger.info "Date added:", "#{document.data['date_added_kb'].class} : #{document.data['date_added_kb'].pretty_inspect}"
   end
 end
